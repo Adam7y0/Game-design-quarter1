ae487bd77bf2e180dccae2b2f063e2ab1614b5e3
Adam7y0 <acodonnell@email.neit.edu>
2024-10-08 16:55:31 -0400

    buttons

---
+++ Week 2/Scripts/Script.js
@@ -0,0 +1,9 @@
+var newMEssage = "John Cena's mad.";
+
+var messageTag = document.getElementById("message");
+
+function showMessageOnPage(){
+    messageTag.innerHTML = newMessage;
+}
+
+showMessageOnPage();
